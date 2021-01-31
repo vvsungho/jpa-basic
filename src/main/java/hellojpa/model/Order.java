72ab50245a57600909adf9ba1ab238d794135e1c
shyoon <shyoon@12cm.co.kr>
2021-01-31 17:17:44 +0900

    20210131 / 연관관계 매핑 기초 - 양방향 연관관계와 연관관계의 주인2 - 주의점, 정리 ~ 연관관계 매핑 기초 - 실전예제2

--- src/main/java/hellojpa/model/Order.java
+++ src/main/java/hellojpa/model/Order.java
@@ -1,66 +1,77 @@
-//package hellojpa.model;
-//
-//import hellojpa.type.OrderStatus;
-//
-//import javax.persistence.*;
-//import java.time.LocalDateTime;
-//
-//@Entity
-//@Table(name = "ORDERS") // order가 db에서 예약어
-//public class Order {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    @Column(name = "order_id")
-//    private Long id;
-//
-//    @Column(name = "member_id")
-//    private Long memberId;
-//
-//    private Member member;
-//
-//    private LocalDateTime orderDate;
-//
-//    @Enumerated(EnumType.STRING)
-//    private OrderStatus orderStatus;
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public Long getMemberId() {
-//        return memberId;
-//    }
-//
-//    public void setMemberId(Long memberId) {
-//        this.memberId = memberId;
-//    }
-//
-//    public LocalDateTime getOrderDate() {
-//        return orderDate;
-//    }
-//
-//    public void setOrderDate(LocalDateTime orderDate) {
-//        this.orderDate = orderDate;
-//    }
-//
-//    public OrderStatus getOrderStatus() {
-//        return orderStatus;
-//    }
-//
-//    public void setOrderStatus(OrderStatus orderStatus) {
-//        this.orderStatus = orderStatus;
-//    }
-//
-//    public Member getMember() {
-//        return member;
-//    }
-//
-//    public void setMember(Member member) {
-//        this.member = member;
-//    }
-//}
+package hellojpa.model;
+
+import hellojpa.type.OrderStatus;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "ORDERS") // order가 db에서 예약어
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItemList = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public List<OrderItem> getOrderItemList() {
+        return orderItemList;
+    }
+
+    // 양방향으로 걸어줌.
+    // order에 orderItem을 넣고 orderItem에 order를 넣어줌.
+    public void setOrderItemList(OrderItem orderItem) {
+        this.getOrderItemList().add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void addOrderItemList() {
+
+    }
+}
